fb3809741e67e0542e79f500f51a392a1042128a
tavolafourcade <tavolafourcade@gmail.com>
2021-01-28 00:12:25 -0500

    Improved responsivness for mobile, iPad and Deskptop sizes

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const app = express();
 
 const path = require('path');
 
+//Estableciendo la ruta de archivos estáticos
 const publicPath = path.resolve(__dirname, './public');
+
 //Haciendo uso de archivos estáticos
 app.use(express.static(publicPath));
 
-app.listen(3000, ()=>{
+app.listen(3030, ()=>{
   console.log('Servidor corriendo en puerto 3000')
 })
 
